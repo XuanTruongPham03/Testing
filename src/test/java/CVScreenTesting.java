62e26d3153f8f78d42a24e6a3889627039922492
Phạm Xuân Trường <phamxuantruong843@gmail.com>
2024-07-22 21:22:56 +0700

    add comments

--- src/test/java/CVScreenTesting.java
+++ src/test/java/CVScreenTesting.java
@@ -6,12 +6,18 @@ import org.junit.jupiter.params.provider.CsvFileSource;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CVScreenTesting {
+    /*
+     * Testcase kiểm thử hộp đen
+     */
     @ParameterizedTest
     @CsvFileSource(resources = "/blackBoxTesting/boundaryTesting.csv", numLinesToSkip = 1)
     public void testBoundaryTesting(double testID, String entryTestRes, double gpa, String expected) {
         assertEquals(expected, CVScreening.screenCV(gpa, entryTestRes));
     }
 
+    /*
+     * Testcase kiểm thử bảng quyết định
+     */
     @ParameterizedTest
     @CsvFileSource(resources = "/blackBoxTesting/decisionTableTesting.csv", numLinesToSkip = 1)
     public void testDecisionTableTesting(double testId, String entryTestRes, double gpa, String expected) {
