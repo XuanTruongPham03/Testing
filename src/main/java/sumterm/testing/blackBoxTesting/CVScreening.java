62edc4543733f2e0fe1b8ed908567ddcfd35c7eb
Phạm Xuân Trường <phamxuantruong843@gmail.com>
2024-07-22 19:08:38 +0700

    update logic

--- src/main/java/sumterm/testing/blackBoxTesting/CVScreening.java
+++ src/main/java/sumterm/testing/blackBoxTesting/CVScreening.java
@@ -12,37 +12,25 @@ public class CVScreening {
      * @return: Kết quả lọc CV của sinh viên
      */
     public static String screenCV(double gpa, String entryTestRes) {
-        // Làm tròn gpa 1 chữ số thập phân hạ xuống
-        gpa = Math.floor(gpa * 10) / 10;
-
-        // Đầu vào không hợp lệ
-        if (!entryTestRes.equals("pass") && !entryTestRes.equals("fail")) {
-            return "invalid input";
-        }
-
         if (gpa < 0 || gpa > 4) {
             return "invalid input";
         }
-
+        
+        if (!entryTestRes.equals("pass") && !entryTestRes.equals("fail")) {
+            return "invalid input";
+        }
+        
         if (gpa < 3.0) {
             return "reject";
         }
-
+        
         if (gpa >= 3.0 && gpa < 3.5) {
-            if (entryTestRes.equals("fail")) {
-                return "reject";
-            } else  {
-                return "phase 1";
-            }
-        } else {
-            if (entryTestRes.equals("fail")) {
-                return "phase 1";
-            } else {
-                return "phase 2";
-            }
+            return entryTestRes.equals("fail") ? "reject" : "phase 1";
         }
-
-        //return "invalid input";
+        
+        // gpa >= 3.5
+        return entryTestRes.equals("fail") ? "phase 1" : "phase 2";
+        
     }
 
     public static void main(String[] args) {
